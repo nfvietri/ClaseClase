7ada23ec9299d98468fe921740d1e217cfab4d9b
Nicolás <nicolasfv90@gmail.com>
2019-08-29 14:03:06 -0300

    ejercicio clase 3 con for

--- Clase_03/src/Clase_03.c
+++ Clase_03/src/Clase_03.c
@@ -10,52 +10,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define NUMEROS_A_PEDIR 6
+
+int cantNros;
 int pedirNumerosYCalcular(int *maximo, int *minimo, float *promedio);
 
 int main(void) {
 	int maximo;
 	int minimo;
 	float promedio;
-	if (pedirNumerosYCalcular(&maximo,&minimo,&promedio)==0){
+	int cantidadNumeros;
+	printf("Cuantos numeros desea ingresar? \n");
+	scanf("%d", &cantNros);
+	if (calcularMaxMinProm(&maximo,&minimo,&promedio)==0){
 		printf("Maximo: %d  Minimo: %d  Promedio: %.2f ",maximo,minimo,promedio);
 	}else{
 		printf("Error.");
 	}
-
-
 	return 0;
 }
 
-int pedirNumerosYCalcular(int *maximo, int *minimo, float *promedio)
+int calcularMaxMinProm(int *maximo, int *minimo, float *promedio)
 {
 	int retorno = 0;
 	int numero;
 	int acumulador = 0;
-	int contador = 0;
 	int maximoAuxiliar;
 	int minimoAuxiliar;
 
-	while (contador < NUMEROS_A_PEDIR)
-	{
-		printf("Ingrese un numero: ");
-		scanf("%d",&numero);
-		if(contador == 0){
-			maximoAuxiliar = numero;
-			minimoAuxiliar = numero;
-		}
-		if (numero > maximoAuxiliar){
-			maximoAuxiliar = numero;
-		}
-		if (numero < minimoAuxiliar){
-			minimoAuxiliar = numero;
-		}
-		acumulador = acumulador + numero;
-		contador++;
+	for(int i = 0;i<cantNros;i++){
+
+            printf("Ingrese un numero \n");
+            scanf("%d", &numero);
+
+            if(i==0){
+                maximoAuxiliar = numero;
+                minimoAuxiliar = numero;
+            } else {
+                if(numero > maximoAuxiliar){
+                    maximoAuxiliar = numero;
+                }
+                if(numero < minimoAuxiliar){
+                    minimoAuxiliar = numero;
+                }
+
+            }
+            acumulador = acumulador + numero;
 	}
+
+
 	*maximo = maximoAuxiliar;
 	*minimo = minimoAuxiliar;
-	*promedio = (float) acumulador / (float) contador;
+	*promedio = (float) acumulador / (float) cantNros;
 	return retorno;
 }
 
